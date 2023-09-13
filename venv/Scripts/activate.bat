12534cbba4d70f92d755bf5e883b7fa879fbfd2c
krrattoss5 <krrattoss5@gmail.com>
2023-09-12 21:45:32 -0500

    correccion de errores basicos

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\MIMIR\Desktop\workspace\proyectos Python\appToDo\venv
+set VIRTUAL_ENV=C:\Users\MIMIR\Desktop\workspace\proyectos Python\todoer\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
